990b746cc0252316079509f280c41f0b117b155d
Lobscob <jeroenvermazeren@outlook.com>
2017-02-13 15:33:06 +0100

    Add a select panel for parameters

---
+++ src/main/display/SelectPanel.java
@@ -0,0 +1,46 @@
+package main.display;
+
+//import display.game.GameFrame;
+
+import main.display.MainFrame;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by jeroen on 2/8/17.
+ */
+public class SelectPanel extends JPanel {
+    private JFrame selectFrame;
+
+    private JButton startButton;
+
+    public SelectPanel(MainFrame mainFrame) {
+        selectFrame = mainFrame;
+
+        startButton = new JButton("Create game!");
+        startButton.addActionListener(new StartButton());
+
+        this.setPreferredSize(new Dimension(370, 125));
+        this.setLayout(new GridBagLayout());
+
+        GridBagConstraints c = new GridBagConstraints();
+
+        c.gridx = 0;
+        c.gridy = 0;
+        this.add(startButton, c);
+
+    }
+
+    private class StartButton implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent actionEvent) {
+            System.out.println("Starts game");
+            new GameFrame();
+            selectFrame.dispose();;
+        }
+    }
+}
